e0420a1e15d38db604bd1d5944e5ec72b473ccd6
cqs1 <61277557+cqs1@users.noreply.github.com>
2023-07-12 15:13:45 +0800

    Update query.php

--- query.php
+++ query.php
@@ -1,15 +1,9 @@
-<?php
-// 获取表单提交的班级和姓名数据
-if(isset($_POST['class']) && isset($_POST['name'])){
-    $class = $_POST['class'];
-    $name = $_POST['name'];
-
-    // 在这里执行你的查询操作或其他逻辑
-    // 可以使用获取到的班级和姓名进行数据库查询、文件读取等操作
-
-    // 示例：输出查询结果
-    echo "班级：$class<br>";
-    echo "姓名：$name<br>";
-    echo "查询结果：XXX"; // 这里可以替换为你的查询结果
+// 在查询成功的条件下，进行页面跳转
+if (mysqli_num_rows($result) > 0) {
+    // 查询成功，进行页面跳转
+    header("Location: new_page.html");
+    exit; // 确保页面跳转后停止执行后续的代码
+} else {
+    // 查询无结果，可以给出相应的提示信息
+    echo "查询无结果";
 }
-?>
