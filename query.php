b4fd31ab68d7b6c3bc71cf34d82004695a89fdc2
cqs1 <61277557+cqs1@users.noreply.github.com>
2023-07-12 15:15:34 +0800

    Update query.php

--- query.php
+++ query.php
@@ -1,9 +1,2 @@
-// 在查询成功的条件下，进行页面跳转
-if (mysqli_num_rows($result) > 0) {
-    // 查询成功，进行页面跳转
-    header("Location: new_page.html");
-    exit; // 确保页面跳转后停止执行后续的代码
-} else {
-    // 查询无结果，可以给出相应的提示信息
-    echo "查询无结果";
-}
+header("Location: new_page.html");
+
